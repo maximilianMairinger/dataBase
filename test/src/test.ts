bd428b7c1d320b79e2f13aca1c5f9a8d7820e9c3
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2019-12-23 02:11:34 +0100

    minor

--- test/src/test.ts
+++ test/src/test.ts
@@ -1,22 +1,5 @@
-import setData, { Data } from "./../../app/src/f-db"
+import { Data } from "./../../app/src/f-db"
 import delay from "delay"
 
-let data = {
-  nested: {
-    prop: "val"
-  },
-  num: 123
-}
 
-
-let db = setData(data)
-
-db.get("num", (num: number) => {
-  console.log("num: " + num)
-})
-
-
-
-delay(1000).then(() => {
-  db.set("num", 1)
-})
+console.log(new Data("asd").toString())
