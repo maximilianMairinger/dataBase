5a6d78bc0ed611107ce4059b2512fe0e75085033
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-01-19 23:37:29 +0100

    work

--- app/src/f-db.ts
+++ app/src/f-db.ts
@@ -1,24 +1,17 @@
-type Subscription<T> = (value: T) => void | Promise<void>
+type Subscription<Values extends any[]> = (...value: Values) => void | Promise<void>
 
-export interface Observable<Value> {
-  get(): Value
-  get(observer?: Subscription<Value>): typeof observer
-  get(cb?: Subscription<Value>): Value | typeof cb
 
-  got(cb: Subscription<Value>): typeof cb
-}
-
-export class Data<Value> implements Observable<Value> {
+export class Data<Value> {
   private value: Value
-  private subsciptions: Subscription<Value>[]
+  private subsciptions: Subscription<[Value]>[]
 
   public constructor(value?: Value) {
     this.value = value
   }
 
   public get(): Value
-  public get(cb: Subscription<Value>, initialize?: boolean): typeof cb
-  public get(observer?: Subscription<Value>, initialize: boolean = false): Value | typeof observer {
+  public get(observer: Subscription<[Value]>, initialize?: boolean): typeof observer
+  public get(observer?: Subscription<[Value]>, initialize: boolean = false): Value | typeof observer {
     if (observer) {
       this.subsciptions.add(observer)
       if (initialize) observer(this.value)
@@ -27,7 +20,7 @@ export class Data<Value> implements Observable<Value> {
     else return this.value
   }
 
-  public got(observer: Subscription<Value>): typeof observer {
+  public got(observer: Subscription<[Value]>): typeof observer {
     this.subsciptions.rmV(observer)
     return observer
   }
@@ -55,5 +48,88 @@ export class Data<Value> implements Observable<Value> {
 }
 
 
+type DataSetify<T> = { 
+  [P in keyof T]: DataSet<T[P]>
+}
 
+
+
+type Tupleify<TupleOrNot> = TupleOrNot extends any[] ? TupleOrNot : [TupleOrNot]
+type Tuple = [...any[]]
+
+
+
+class DataCollection<Value_Values, Values extends Tupleify<Value_Values> = Tupleify<Value_Values>> {
+  private observers: Subscription<Values>[] = []
+  private datas: DataSetify<Values>
+  constructor(...datas: DataSetify<Values>) {
+    this.datas = datas
+  }
+
+
+  private subscribe(observer: Subscription<Values>, initialize: boolean) {
+    this.observers.add(observer)
+    if (initialize) observer(...this.get())
+  }
+
+  public get(): Values
+  public get(observer: Subscription<Values>, initialize?: boolean): DataSubscription<Values>
+  public get(observer?: Subscription<Values>, initialize: boolean = true): DataSubscription<Values> | Values[] {
+    //@ts-ignore
+    if (observer === undefined) return this.datas.Inner("get", [])
+    else return new DataSubscription(this, observer, true, initialize)
+  }
+  public got(observer: Subscription<Values>): Subscription<Values> {
+    throw new Error("Method not implemented.")
+  }
+
+} 
+
+
+
+type DataSet<Value> = Data<Value> | DataCollection<Value>
+
+
+let ssssssssss: DataSet<[true, "false"]> = new DataCollection(new Data(true), new Data("false"))
+ssssssssss.get((s, w) => {
+  s
+})
+
+class DataSubscription<Value_Values, Values extends Tupleify<Value_Values> = Tupleify<Value_Values>> {
+  private _active: boolean = false
+
+  constructor(data: DataSet<Value_Values>, subscription: Subscription<Values>, activate?: false)
+  constructor(data: DataSet<Value_Values>, subscription: Subscription<Values>, activate?: true, inititalize?: boolean)
+  constructor(private _data: DataSet<Value_Values>, private _subscription: Subscription<Values>, activate?: boolean, inititalize?: boolean) {
+    //@ts-ignore
+    this.active(activate, inititalize)
+  }
+
+  public acivate(initialize: boolean = true): this {
+    if (this._active) return
+    this._active = true
+
+    this._data.get(this._subscription, initialize)
+
+
+
+    return this
+  }
+
+  public deacivate(): this {
+    if (!this._active) return
+    this._active = false
+    
+    return this
+  }
+
+  public active(activate: false): this
+  public active(activate: true, initialize?: boolean): this
+  public active(activate: boolean, initialize?: boolean): this {
+    if (activate) this.acivate(initialize)
+    else this.deacivate()
+    return this
+  }
+  
+}
 
